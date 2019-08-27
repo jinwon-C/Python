141d51b37ed4624571ae6fc4ae3b2dc42a1cbe69
jinwon <jinwontoo@naver.com>
2019-08-27 17:36:08 +0900

    Do not change

--- csv/change.py
+++ csv/change.py
@@ -1,12 +1,12 @@
 import os
 import pandas as pd
-fpath = 'C:\\Users\\Tony\\Desktop\\git\\Data\\2019-08-16\\Audio\\Numeric\\1'
+fpath = 'C:\\Users\\Tony\\Desktop\\git\\Data\\2019-08-16\\Dual\\1'
 sorted_flist = sorted(os.listdir(fpath))
 for i, file in enumerate(sorted_flist):
-    with open('C:\\Users\\Tony\\Desktop\\git\\Data\\2019-08-16\\Audio\\1\\1_%d'%(i+1)+'.csv', 'r') as f:
+    with open('C:\\Users\\Tony\\Desktop\\git\\Data\\2019-08-16\\Dual\\1\\1_%d'%(i+1)+'.csv', 'r') as f:
         a_list = pd.read_csv(f, header=None)
-        a_list.loc[:,0] = 10
-        print(file)
-        print(a_list.loc[:,0])
+        #a_list.loc[:,0] = 10
+        print(file, i)
+        #print(a_list.loc[:,0])
 
         #a_list.to_csv('C:\\Users\\Tony\\Desktop\\git\\Data\\2019-08-16\\Audio\\1\\1_%d'%(i+1)+'.csv', header=None, index=False)
