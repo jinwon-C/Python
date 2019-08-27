de23e0296f5caf00931f4818f882f45891912a97
jinwon <jinwontoo@naver.com>
2019-08-27 16:56:08 +0900

    Modify to numeric file name

--- rename/rename.py
+++ rename/rename.py
@@ -2,11 +2,11 @@ import os
 from os.path import join
 
 #fpath = '\\Users\\Tony\\Desktop\\paper\\Data_file\\without_noise\\raw_data\\nothing' #폴더 이름
-fpath = '\\Users\\Tony\\Desktop\\depth_classification\\low-high'
+fpath = 'C:\\Users\\Tony\\Desktop\\git\\Data\\2019-08-16\\Audio\\Numeric\\a'
 sorted_flist = sorted(os.listdir(fpath))
 for i,file in enumerate(sorted_flist):
     print(file)
-    #new_file = file.replace('note8_20K_','') #'기존 이름', '바꿀 이름'
-    new_file = "low-high%d"%(i+1)+".wav"
+    #new_file = file.replace('1_','a_') #'기존 이름', '바꿀 이름'
+    new_file = "a_%d"%(i+1)+".wav"
     print(new_file)
     os.rename(join(fpath,file), join(fpath,new_file))
